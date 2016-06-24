02289d6f3d866ac71c67977490340022b85f0a9d
mshr-h <mshr-h@users.noreply.github.com>
2016-06-24 16:16:31 +0900

    add print

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Brainfxck struct {
 	dp      int
 	ip      int
-	memory  [16]int
+	memory  []int
 	program string
 }
 
@@ -13,66 +16,96 @@ func loadProgram(program string) *Brainfxck {
 	var bf Brainfxck
 	bf.dp = 0
 	bf.ip = 0
+	bf.memory = make([]int, 256)
 	bf.program = program
 	return &bf
 }
 
 func (bf *Brainfxck) execute() {
-	for len(bf.program) > bf.ip {
-		switch bf.program[bf.ip] {
-		case '+':
-			bf.memory[bf.dp]++
-		case '-':
-			bf.memory[bf.dp]--
-		case '>':
-			bf.dp++
-		case '<':
-			bf.dp--
-		case '.':
-			fmt.Printf("%c", bf.memory[bf.dp])
-		case '[':
-			{
-				if bf.memory[bf.dp] == 0 {
-					var count = 1
-					for count > 0 {
-						bf.ip++
-						switch bf.program[bf.ip] {
-						case '[':
-							count++
-						case ']':
-							count--
-						}
-					}
-				}
-			}
-		case ']':
-			{
+	switch bf.program[bf.ip] {
+	case '+':
+		bf.memory[bf.dp]++
+	case '-':
+		bf.memory[bf.dp]--
+	case '>':
+		bf.dp++
+	case '<':
+		bf.dp--
+	case '.':
+		fmt.Printf("%c", bf.memory[bf.dp])
+	case '[':
+		{
+			if bf.memory[bf.dp] == 0 {
 				var count = 1
 				for count > 0 {
-					bf.ip--
+					bf.ip++
 					switch bf.program[bf.ip] {
 					case '[':
-						count--
-					case ']':
 						count++
+					case ']':
+						count--
 					}
 				}
-				bf.ip--
 			}
 		}
-		bf.ip++
+	case ']':
+		{
+			var count = 1
+			for count > 0 {
+				bf.ip--
+				switch bf.program[bf.ip] {
+				case '[':
+					count--
+				case ']':
+					count++
+				}
+			}
+			bf.ip--
+		}
+	}
+	bf.ip++
+}
+
+func (bf *Brainfxck) run() {
+	for bf.ip < len(bf.program) {
+		bf.execute()
+	}
+}
+
+func (bf *Brainfxck) print() {
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("dp      :", bf.dp)
+	fmt.Println("program :", bf.program)
+	fmt.Println("         ", strings.Repeat(" ", bf.ip-2), "^")
+	fmt.Println("address         content")
+	printMemory(bf.memory)
+}
+
+func printMemory(memory []int) {
+
+	/*
+		0x0000 00 24 00 00 00 00 00 00
+	*/
+	fmt.Printf("      ")
+	for i := 0; i < 16; i++ {
+		fmt.Printf(" %2X", i)
+	}
+	fmt.Println()
+
+	for i := 0; i < len(memory); i += 16 {
+		fmt.Printf("0x%.4X ", i)
+		for _, v := range memory[i : i+16] {
+			fmt.Printf("%.2X ", v)
+		}
+		fmt.Println()
 	}
 }
 
 func main() {
 	var bf = loadProgram("++++++[>++++++<-]>.")
-	bf.execute()
+	bf.run()
+	bf.print()
 
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("ip      :", bf.ip)
-	fmt.Println("dp      :", bf.dp)
-	fmt.Println("program :", bf.program)
-	fmt.Println("memory  :", bf.memory)
 	return
 }
